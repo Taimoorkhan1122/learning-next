83f1f111a54287182aa035f76f0ecb3fd0b643d1
taimoorkhan1122 <khant8k83@gmail.com>
2022-05-15 19:18:22 +0500

    next js starter

---
+++ pages/cars/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <div>Welcom to Cars sections</div>
+      <ul>
+        <li>Lambo</li>
+        <li>Tesla</li>
+        <li>Mercedes</li>
+      </ul>
+    </div>
+  );
+};
+
+export default index;
