62ce24f53e73129892ca916f5b5c6fdaf2b1650d
taimoorkhan1122 <khant8k83@gmail.com>
2022-05-24 21:53:48 +0500

    get static props done

--- pages/cars/index.jsx
+++ pages/cars/index.jsx
@@ -1,16 +1,42 @@
+import Link from "next/link";
 import React from "react";
 
-const index = () => {
+const index = ({ cars }) => {
+  console.log(cars);
   return (
     <div>
       <div>Welcom to Cars sections</div>
       <ul>
-        <li>Lambo</li>
-        <li>Tesla</li>
-        <li>Mercedes</li>
+        {cars.map((car, id) => (
+          <Link
+            key={`id+${car.model}`}
+            href={`${car.manufacturere}?${car.model}`}
+          >
+            <li>
+              <div>{car.manufacturere}</div>
+              <div>{car.model}</div>
+              <div>{car.color}</div>
+            </li>
+          </Link>
+        ))}
       </ul>
     </div>
   );
+};
+
+export const getStaticProps = () => {
+  const cars = [
+    { manufacturere: "Audi", model: "A8", color: "Vibrant Blue" },
+    { manufacturere: "Ferrari", model: "F920", color: "Signature Red" },
+    { manufacturere: "Audi", model: "G47", color: "Black" },
+    { manufacturere: "Mercedez", model: "Mercedez Benz v11", color: "Gray" },
+  ];
+
+  return {
+    props: {
+      cars,
+    },
+  };
 };
 
 export default index;
